4611f99df3dddf96c158bfa39322788539c74882
tnishiz <85749872+tnishiz@users.noreply.github.com>
2022-05-05 23:27:06 +0900

    6-36 Finished

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "にしやん",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "12345@example.com",
-  phone: "090-1111-2222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
-  );
+  return <Router />;
 }
